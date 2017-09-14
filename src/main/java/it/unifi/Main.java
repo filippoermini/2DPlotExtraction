28f40538bb0f44fb32fe1d9c5773768ecf000beb
filippo.ermini <filippo.ermini@gmail.com>
2017-09-14 16:33:48 +0200

    Importazione vecchio progetto progress #1

--- src/main/java/it/unifi/Main.java
+++ src/main/java/it/unifi/Main.java
@@ -3,6 +3,7 @@ package it.unifi;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("tesi specialistica");
+        System.out.println("Tesi Specialistica");
+
     }
 }
