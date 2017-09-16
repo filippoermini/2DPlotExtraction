281ac3965f6db28cff03085b6877b75295d215bc
filippo.ermini <filippo.ermini@gmail.com>
2017-09-16 10:46:17 +0200

    Riallineamento e maven install Close #2

--- src/main/java/it/unifi/Main.java
+++ src/main/java/it/unifi/Main.java
@@ -1,9 +1,16 @@
 package it.unifi;
 
+import org.bytedeco.javacpp.opencv_core;
+import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+
+
+
+
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Tesi Specialistica");
+        opencv_core.IplImage image = cvLoadImage("graph.png",CV_LOAD_IMAGE_GRAYSCALE);
 
     }
 }
