4b78dcc025208bafe438953381f9450bd57f6019
filippo.ermini <filippo.ermini@gmail.com>
2017-11-14 21:49:33 +0100

    Aggiunta di tess4j

--- src/main/java/it/unifi/Main.java
+++ src/main/java/it/unifi/Main.java
@@ -1,6 +1,14 @@
 package it.unifi;
 
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+
+import java.io.File;
+
 import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 
 
@@ -9,8 +17,32 @@ import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Tesi Specialistica");
+
+        //----------//
+        //  OpenCV  //
+        //----------//
+
         opencv_core.IplImage image = cvLoadImage("graph.png",CV_LOAD_IMAGE_GRAYSCALE);
+        CanvasFrame canvas = new CanvasFrame("Test", 1); // gamma=1
+        canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        canvas.setCanvasSize(100, 100);
+        OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+        canvas.showImage(converter.convert(image));
+
+        //----------//
+        //  Tess4J  //
+        //----------//
+
+        File fileImage = new File("graph.png");
+        ITesseract instance = new Tesseract();
+
+        try{
+            String result = instance.doOCR(fileImage);
+            System.out.println(result);
+
+        }catch(TesseractException e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
