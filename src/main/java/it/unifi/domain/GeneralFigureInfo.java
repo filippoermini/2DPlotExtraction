f284cedea9f2e3a66dab2edc6c8b637c9947889f
filippo.ermini <filippo.ermini@gmail.com>
2018-04-12 12:19:27 +0200

    aggiunta file

---
+++ src/main/java/it/unifi/domain/GeneralFigureInfo.java
@@ -0,0 +1,14 @@
+package it.unifi.domain;
+
+public class GeneralFigureInfo {
+
+	private BoxInfo title;
+	private AxisInfo x_axis;
+	private BoxInfo legend;
+	private ValueInfo x_gridlines;
+	private Object figure_info;
+	private ValueInfo y_gridlines;
+	private AxisInfo y_axis;
+	private Bbox plot_info;
+	
+}
