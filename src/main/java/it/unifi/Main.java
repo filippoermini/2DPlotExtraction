535544c457e47905f7c76657a58b3001bd62cf2f
filippo.ermini <filippo.ermini@gmail.com>
2018-06-10 14:49:19 +0200

    serializzazione in json completata

--- src/main/java/it/unifi/Main.java
+++ src/main/java/it/unifi/Main.java
@@ -4,42 +4,70 @@ import it.unifi.annotation.Parser;
 import it.unifi.domain.AnnotationLine;
 import it.unifi.domain.LineGraphList;
 import it.unifi.export.SampleMatrix;
+import it.unifi.export.SampleMatrixList;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
+import com.google.gson.Gson;
+
+
+
 
 public class Main {
 
+	
     public static void main(String[] args){
 
-    	LineGraphList graphList = Parser.readStream("annotations.json");
-   
-        //----------------------------------//
-    	//  Statistiche sul numero di linee	//
-    	//----------------------------------//
-    	ArrayList<SampleMatrix> matrixList = new ArrayList();
-    	HashMap<Integer, LineGraphList> lineMap = new HashMap<Integer, LineGraphList>();
-    	Iterator<AnnotationLine> it = graphList.getLineGraph().iterator();
-    	AnnotationLine annotationLine;
-    	while(it.hasNext()) {
-    		annotationLine = it.next();
-    		//inizializzo la matrice dei campionamenti
-    		int models = annotationLine.getModels().length;
-    		int samples = annotationLine.getModels()[0].getBbox().length;
-    		SampleMatrix sampleMatrix = new SampleMatrix(samples, models);
-    		//popolo la matrice con i campionamenti
-    		sampleMatrix.setSampleMatrix(annotationLine);
-    		if(lineMap.get(annotationLine.getModels().length) == null) {
-    			lineMap.put(annotationLine.getModels().length, new LineGraphList());
-    		}
-    		lineMap.get(annotationLine.getModels().length).getLineGraph().add(annotationLine);
-    	}
-    	Iterator itMap = lineMap.entrySet().iterator();
-    	while (itMap.hasNext()) {
-	        HashMap.Entry<Integer,LineGraphList> pair = (HashMap.Entry)itMap.next();
+	    	LineGraphList graphList = Parser.readStream("annotations.json");
+	    	
+	    //-----------------------------------//
+	    	//  Statistiche sul numero di linee	//
+	    	//-----------------------------------//
+	    
+	    	HashMap<Integer, LineGraphList> lineMap = new HashMap<Integer, LineGraphList>();
+	    	Iterator<AnnotationLine> it = graphList.getLineGraph().iterator();
+	    	AnnotationLine annotationLine;
+	    	while(it.hasNext()) {
+	    		annotationLine = it.next();
+	    		
+	    		if(lineMap.get(annotationLine.getModels().length) == null) {
+	    			lineMap.put(annotationLine.getModels().length, new LineGraphList());
+	    		}
+	    		lineMap.get(annotationLine.getModels().length).getLineGraph().add(annotationLine);
+	    		
+	    	}
+	    	Iterator itMap = lineMap.entrySet().iterator();
+	    	while (itMap.hasNext()) {
+	    		SampleMatrixList matrixList = new SampleMatrixList();
+	    		HashMap.Entry<Integer,LineGraphList> pair = (HashMap.Entry)itMap.next();
 	        System.out.println("Linee " + pair.getKey() + " : " + pair.getValue().getLineGraph().size()+" Grafici");
-    		}
-    }
+	        Iterator<AnnotationLine> itGraph = pair.getValue().getLineGraph().iterator();
+	    		AnnotationLine annotationLineGraph;
+	    		while(itGraph.hasNext()) {
+	    			annotationLineGraph = itGraph.next();
+	    			//inizializzo la matrice dei campionamenti
+		    		int models = annotationLineGraph.getModels().length;
+		    		int samples = annotationLineGraph.getModels()[0].getBbox().length;
+		    		SampleMatrix sampleMatrix = new SampleMatrix(samples, models);
+		    		//popolo la matrice con i campionamenti
+		    		sampleMatrix.setSampleMatrix(annotationLineGraph);
+		    		matrixList.addMatrix(sampleMatrix);
+			}
+		    String json = matrixList.serialize();
+		    try {
+				PrintWriter out = new PrintWriter("serialize"+pair.getKey()+"Line.json");
+				out.print(json);
+				out.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	}
+	}
+    
+ 
 }
--- src/main/java/it/unifi/export/SampleMatrix.java
+++ src/main/java/it/unifi/export/SampleMatrix.java
@@ -1,5 +1,11 @@
 package it.unifi.export;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang.ArrayUtils;
+
 import it.unifi.domain.AnnotationLine;
 import it.unifi.domain.Model;
 
@@ -21,12 +27,23 @@ public class SampleMatrix {
 			Model model = annotation.getModels()[i];
 			for(int j=0;j<model.getBbox().length;j++) {
 				//i campionamenti sono sempre uno in più
-				double max = Math.max((Double) model.getY()[0],(Double) model.getY()[model.getY().length-1]);
-				double min = Math.min((Double) model.getY()[0],(Double) model.getY()[model.getY().length-1]);
-				double value = ((Double) model.getY()[j] + (Double) model.getY()[j+1]) / 2.0; 
-				double nomalizedValue = (value -min) / (max-min);
+				
+				double max = Double.MIN_VALUE;
+				double min = Double.MAX_VALUE;
+				for(int k=0;k<model.getY().length;k++) {
+					max = (Double) ((Double) model.getY()[k] > max?model.getY()[k]:max);
+					min = (Double) ((Double) model.getY()[k] < min?model.getY()[k]:min);
+				}
+				double normalizedValue;
+				if(max == min) {
+					//retta orizzontale
+					normalizedValue = 1;
+				}else {
+					double value = ((Double) model.getY()[j] + (Double) model.getY()[j+1]) / 2.0; 
+					normalizedValue = (value -min) / (max-min);
+				}
 				this.sampleArray[j].setBoxAtIndex(model.getBbox()[j], i);
-				this.sampleArray[j].setValueAtIndex(nomalizedValue, i);
+				this.sampleArray[j].setValueAtIndex(normalizedValue, i);
 			}
 		}
 	}
