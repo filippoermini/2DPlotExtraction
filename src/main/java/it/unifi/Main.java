bce1a35f1dcc886190a13c03a8c60dad23c55143
filippo.ermini <filippo.ermini@gmail.com>
2018-10-24 09:11:18 +0200

    file input multipli

--- src/main/java/it/unifi/Main.java
+++ src/main/java/it/unifi/Main.java
@@ -19,43 +19,76 @@ import com.google.gson.Gson;
 
 public class Main {
 
-	
     public static void main(String[] args){
-
-	    	LineGraphList graphList = Parser.readStream("annotations.json",false);
-	    	
-	    	//-----------------------------------//
-	    	//  Statistiche sul numero di linee	//
-	    	//-----------------------------------//
-	    
-	    	HashMap<Integer, LineGraphList> lineMap = new HashMap<Integer, LineGraphList>();
-	    	Iterator<AnnotationLine> it = graphList.getLineGraph().iterator();
-	    	AnnotationLine annotationLine;
-	    	while(it.hasNext()) {
-	    		annotationLine = it.next();
-	    		
-	    		if(lineMap.get(annotationLine.getModels().length) == null) {
-	    			lineMap.put(annotationLine.getModels().length, new LineGraphList());
-	    		}
-	    		lineMap.get(annotationLine.getModels().length).getLineGraph().add(annotationLine);
-	    		
-	    	}
-	    	Iterator itMap = lineMap.entrySet().iterator();
-	    	while (itMap.hasNext()) {
-	    		SampleMatrixObject matrixList = new SampleMatrixObject();
-	    		HashMap.Entry<Integer,LineGraphList> pair = (HashMap.Entry)itMap.next();
-	    		System.out.println("Linee " + pair.getKey() + " : " + pair.getValue().getLineGraph().size()+" Grafici");
-	    		Iterator<AnnotationLine> itGraph = pair.getValue().getLineGraph().iterator();
-	    		AnnotationLine annotationLineGraph;
-	    		while(itGraph.hasNext()) {
-	    			annotationLineGraph = itGraph.next();
-	    			//inizializzo la matrice dei campionamenti
-		    		int models = annotationLineGraph.getModels().length;
-		    		int samples = annotationLineGraph.getModels()[0].getBbox().length;
-		    		SampleMatrix sampleMatrix = new SampleMatrix(samples, models);
-		    		//popolo la matrice con i campionamenti
-		    		sampleMatrix.setSampleMatrix(annotationLineGraph);
-		    		matrixList.addMatrix(sampleMatrix);
+    	
+    	boolean legend = true;
+		String[] jsonFiles = new String[]{};
+		String[] imageDir  = new String[]{};
+		String destinationDir = "";
+    	if(args.length==0){
+            System.out.println("Arguments requires\n");
+            System.exit(-1);
+        }
+    	if(args.length > 0){
+    		String arg;
+    		int i = 0;
+ 
+    		while (i < args.length && args[i].startsWith("-")) {
+                arg = args[i++];
+                if(arg.contains("-l")){
+                	legend = false;
+                }
+                if(arg.contains("-j")){
+                	if (i < args.length){
+                		jsonFiles = args[i++].split(",");
+                	}
+                }
+                if(arg.contains("-d")){
+                	if (i < args.length){
+                		imageDir = args[i++].split(",");
+                	}
+                }
+                if(arg.contains("-D")){
+                	if(i < args.length){
+                		destinationDir = args[i++];
+                	}
+                }
+    		}
+    	}
+	    LineGraphList graphList = Parser.readStream(legend,jsonFiles,imageDir,destinationDir);
+    	
+    	//-----------------------------------//
+    	//  Statistiche sul numero di linee	//
+    	//-----------------------------------//
+    
+    	HashMap<Integer, LineGraphList> lineMap = new HashMap<Integer, LineGraphList>();
+    	Iterator<AnnotationLine> it = graphList.getLineGraph().iterator();
+    	AnnotationLine annotationLine;
+    	while(it.hasNext()) {
+    		annotationLine = it.next();
+    		
+    		if(lineMap.get(annotationLine.getModels().length) == null) {
+    			lineMap.put(annotationLine.getModels().length, new LineGraphList());
+    		}
+    		lineMap.get(annotationLine.getModels().length).getLineGraph().add(annotationLine);
+    		
+    	}
+    	Iterator itMap = lineMap.entrySet().iterator();
+    	while (itMap.hasNext()) {
+    		SampleMatrixObject matrixList = new SampleMatrixObject();
+    		HashMap.Entry<Integer,LineGraphList> pair = (HashMap.Entry)itMap.next();
+    		System.out.println("Linee " + pair.getKey() + " : " + pair.getValue().getLineGraph().size()+" Grafici");
+    		Iterator<AnnotationLine> itGraph = pair.getValue().getLineGraph().iterator();
+    		AnnotationLine annotationLineGraph;
+    		while(itGraph.hasNext()) {
+    			annotationLineGraph = itGraph.next();
+    			//inizializzo la matrice dei campionamenti
+	    		int models = annotationLineGraph.getModels().length;
+	    		int samples = annotationLineGraph.getModels()[0].getBbox().length;
+	    		SampleMatrix sampleMatrix = new SampleMatrix(samples, models);
+	    		//popolo la matrice con i campionamenti
+	    		sampleMatrix.setSampleMatrix(annotationLineGraph);
+	    		matrixList.addMatrix(sampleMatrix);
 			}
 		    String json = matrixList.serialize();
 		    try {
@@ -66,7 +99,7 @@ public class Main {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-	    	}
+    	}
 	}
     
  
--- src/main/java/it/unifi/annotation/Parser.java
+++ src/main/java/it/unifi/annotation/Parser.java
@@ -5,7 +5,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -22,44 +29,84 @@ import it.unifi.export.SampleMatrix;
 
 public class Parser {
 
-	public static LineGraphList readStream(String fileName,boolean legend) {
+	public static LineGraphList readStream(boolean legend,String[] jsonAnnotationFiles, String[] imageDirs, String destinationDir) {
 		LineGraphList graphList = new LineGraphList();
 		try {
-	    	
-	    	FileInputStream isInputStream = new FileInputStream(new File(fileName));
-	        JsonReader reader = new JsonReader(new InputStreamReader(isInputStream, "UTF-8"));
-	        Gson gson = new GsonBuilder().create();
-
-	        // Read file in stream mode
-	        reader.beginArray();
-	        while (reader.hasNext()) {
-	            // Read data into object model
-	            try {
-	            	AnnotationLine annotationLine = gson.fromJson(reader, AnnotationLine.class);
-	            	if(annotationLine.getType().contentEquals("line")) {
-	            		if(!legend){
-	            			//controllo se la legenda è interna la grafico 
-	            			GeneralFigureInfo plotInfo = (GeneralFigureInfo) annotationLine.getGeneral_figure_info();
-	            			Bbox plotBox = plotInfo.getPlot_info().getBbox();
-	            			Bbox legendBox = plotInfo.getLegend().getBbox();
-	            			if(!Bbox.isInternal(plotBox, legendBox)){
-	            				graphList.getLineGraph().add(annotationLine);
+			int index = 0;
+			int prog = 1;
+			
+			File destFolder = new File(destinationDir);
+			if(!destFolder.exists()){
+				destFolder.mkdirs();
+			}
+			
+	    	for(String json: jsonAnnotationFiles){
+	    		String imageDir = imageDirs[index++];
+		    	FileInputStream isInputStream = new FileInputStream(new File(json));
+		        JsonReader reader = new JsonReader(new InputStreamReader(isInputStream, "UTF-8"));
+		        Gson gson = new GsonBuilder().create();
+	
+		        // Read file in stream mode
+		        reader.beginArray();
+		        while (reader.hasNext()) {
+		            // Read data into object model
+		            try {
+		            	AnnotationLine annotationLine = gson.fromJson(reader, AnnotationLine.class);
+		            	if(annotationLine.getType().contentEquals("line")) {
+		            		//controllo l'esistenza dell'immagine
+		            		File image = new File(imageDir+"/"+annotationLine.getImage_index()+".png");
+	            			if(image.exists()){
+	            				
+			            		if(!legend){
+			            			//controllo se la legenda è interna la grafico 
+			            			GeneralFigureInfo plotInfo = (GeneralFigureInfo) annotationLine.getGeneral_figure_info();
+			            			Bbox plotBox = plotInfo.getPlot_info().getBbox();
+			            			Bbox legendBox = plotInfo.getLegend().getBbox();
+			            			if(!Bbox.isInternal(plotBox, legendBox)){
+			            				graphList.getLineGraph().add(annotationLine);
+			            				//sposto l'immagine nella cartella di destinazione e modifico l'indice dell'immagine
+			            				String pathTO = destinationDir + "/" + prog +".png";
+			            				String pathFrom = imageDir+"/"+annotationLine.getImage_index()+".png";
+			            				copyFile(pathFrom, pathTO);
+			            				annotationLine.setImage_index(prog++);
+			            				System.out.println("immagine "+pathTO+" creata");
+			            			}
+			            		}else{
+			            			graphList.getLineGraph().add(annotationLine);
+			            			//sposto l'immagine nella cartella di destinazione e modifico l'indice dell'immagine
+		            				String pathTO = destinationDir + "/" + prog +".png";
+		            				String pathFrom = imageDir+"/"+annotationLine.getImage_index()+".png";
+		            				copyFile(pathFrom, pathTO);
+		            				System.out.println("immagine "+pathTO+" creata");
+		            				annotationLine.setImage_index(prog++);
+		            				
+			            		}
 	            			}
-	            			
-	            		}
-	            		
-	            		
-	            	}
-	            }catch(Exception e) {
-	            	System.out.println(e.getLocalizedMessage());
-	            }
-	        }
-	        reader.close();
-	    } catch (UnsupportedEncodingException ex) {
-	        
+		            	}
+		            }catch(Exception e) {
+		            	System.out.println(e.getLocalizedMessage());
+		            }
+		        }
+		        reader.close();
+		        System.out.println("immagini elaborate: "+prog);
+	    	}
+		   } catch (UnsupportedEncodingException ex) {
+			   System.err.println(ExceptionUtils.getStackTrace(ex));
 	    } catch (IOException ex) {
-	        
+	    	System.err.println(ExceptionUtils.getStackTrace(ex));
 	    }
 	    return graphList;
 	}
+	
+	public static void copyFile(String origin, String destination) throws IOException {
+        Path FROM = Paths.get(origin);
+        Path TO = Paths.get(destination);
+        //overwrite the destination file if it exists, and copy
+        // the file attributes, including the rwx permissions
+        CopyOption[] options = new CopyOption[]{
+          StandardCopyOption.REPLACE_EXISTING,
+          StandardCopyOption.COPY_ATTRIBUTES
+        }; 
+        Files.copy(FROM, TO, options);
+    }
 }
